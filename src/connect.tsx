74ad1e505ed4e7c9d18c461aae9b255e1b5c6ffd
yuity0725 <cherryblossoms0725@gmail.com>
2023-07-10 10:57:57 +0900

    cors解消

--- src/connect.tsx
+++ src/connect.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import QueryString from "qs";
 
 export const postAnswers = (
   targetId: number,
@@ -6,45 +7,34 @@ export const postAnswers = (
   answers: number[]
 ) => {
   const url =
-    "https://script.google.com/macros/s/AKfycbzDofx2PIqbLVdBHw39GcG7yLFJTwTWgVb0GOl33YuAdaD_fr1GVlI93sYGB51xVJq35g/exec";
+    "https://script.google.com/macros/s/AKfycbx3BG9LhRINTI2aUD9hPJXAJvPG87641NdUE74mtljCJGGuYXiVn-pA5nWhKdijCwd-tg/exec";
   axios.post(
     url,
-    {
+    QueryString.stringify({
       action: "insert",
       sheetName: "human",
-      rows: [
-        {
-          "date-time": new Date(),
-          target_ID: targetId,
-          ID_of_image0: sampleImgs[0],
-          score_of_image0: answers[0],
-          ID_of_image1: sampleImgs[1],
-          score_of_image1: answers[1],
-          ID_of_image2: sampleImgs[2],
-          score_of_image2: answers[2],
-          ID_of_image3: sampleImgs[3],
-          score_of_image3: answers[3],
-          ID_of_image4: sampleImgs[4],
-          score_of_image4: answers[4],
-          ID_of_image5: sampleImgs[5],
-          score_of_image5: answers[5],
-          ID_of_image6: sampleImgs[6],
-          score_of_image6: answers[6],
-          ID_of_image7: sampleImgs[7],
-          score_of_image7: answers[7],
-          ID_of_image8: sampleImgs[8],
-          score_of_image8: answers[8],
-          ID_of_image9: sampleImgs[9],
-          score_of_image9: answers[9],
-        },
-      ],
-    },
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-    }
+      date_time: new Date(),
+      target_ID: targetId,
+      ID_of_image0: sampleImgs[0],
+      score_of_image0: answers[0],
+      ID_of_image1: sampleImgs[1],
+      score_of_image1: answers[1],
+      ID_of_image2: sampleImgs[2],
+      score_of_image2: answers[2],
+      ID_of_image3: sampleImgs[3],
+      score_of_image3: answers[3],
+      ID_of_image4: sampleImgs[4],
+      score_of_image4: answers[4],
+      ID_of_image5: sampleImgs[5],
+      score_of_image5: answers[5],
+      ID_of_image6: sampleImgs[6],
+      score_of_image6: answers[6],
+      ID_of_image7: sampleImgs[7],
+      score_of_image7: answers[7],
+      ID_of_image8: sampleImgs[8],
+      score_of_image8: answers[8],
+      ID_of_image9: sampleImgs[9],
+      score_of_image9: answers[9],
+    })
   );
 };
